929fb372fd78fe4d37ce650a7ad3db22ff5a7bcd
lgkpba <lgkpba@gmail.com>
2025-02-04 16:57:25 -0300

    tela de cadastrar pessoas

--- interfaces.py
+++ interfaces.py
@@ -1,15 +1,62 @@
 import PySimpleGUI as sg
 
+def cadastrar_pessoa():
+    layout = [[sg.Text("Cadastro de Pessoa")],
+              [sg.Text("CPF", size=(15,1)), sg.InputText(key='cpf', size=(20,1))],
+              [sg.Text("Nome", size=(15,1)), sg.InputText(key='nome', size=(20,1))],
+              [sg.Text("Data de nascimento", size=(15,1)), sg.InputText(key='nascimento', size=(20,1))],
+              [sg.Text("Endereço", size=(15,1)), sg.InputText(key='endereço', size=(20,1))],
+              [sg.Text("Telefone", size=(15,1)), sg.InputText(key='telefone', size=(20,1))],
+              [sg.Radio("Guia", "guia", default=True, size=(10,1)), sg.Radio("Cliente", "cliente", default=False, size=(10,1))],
+              [sg.Button("Salvar"), sg.Button("Voltar")]]
+    
+    window = sg.Window("Cadastrar Pessoa", layout)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Voltar":
+            break
+        elif event == "Salvar":
+            sg.popup(f"Pessoa {values['nome']} cadastrada!")
+
+    window.close()
+
+def cadastrar_hotel():
+    layout = [[sg.Text("Cadastro de Hotel")],
+              [sg.InputText(key='hotel', size=(20,1))],
+              [sg.Button("Salvar"), sg.Button("Voltar")]]
+    
+    window = sg.Window("Cadastrar Hotel", layout)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Voltar":
+            break
+        elif event == "Salvar":
+            sg.popup(f"Hotel {values['hotel']} cadastrado!")
+
+    window.close()
+
 def menu():
     layout = [[sg.Text("Selecione o que deseja fazer")],
-              [sg.Button("Cadastrar Pessoa"), sg.Button("Cadastrar Hotel")]]
+              [sg.Button("Cadastrar Pessoa"), sg.Button("Cadastrar Hotel")],
+              [sg.Button("Sair")]]
 
     window = sg.Window("BD", layout)
 
     while True:
         event, values = window.read()
-        if event == sg.WIN_CLOSED or event == "Cancel":
+        if event == sg.WIN_CLOSED or event == "Sair":
             break
+        elif event == "Cadastrar Pessoa":
+            window.hide()  # Esconde a tela principal
+            cadastrar_pessoa()
+            window.un_hide()  # Reexibe a tela principal ao voltar
+        elif event == "Cadastrar Hotel":
+            window.hide()
+            cadastrar_hotel()
+            window.un_hide()
 
     window.close()
+
 menu()
